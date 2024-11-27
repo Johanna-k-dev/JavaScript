7aa6edbdf274afd50fdc4a0ea310a62397536ce2
Johanna Keïta <veille.technologique.jk@gmail.com>
2024-11-27 13:43:22 +0100

    variable naming

---
+++ Les-variables/Script.js
@@ -0,0 +1,26 @@
+// Renommer les variables correctement
+
+
+
+// prénom = "Bruno";
+
+const surName= "Bruno"
+// let z = "Salut je m'appelle Bruno et voici ma description";
+
+let description = "Salut je m'appelle Bruno et voici ma description"
+// const métier = "Analyste financier au Yémen";
+
+const job = "Analyste financier au Yémen"
+// const age = 23;
+
+let age = 23;
+// l = "Bordeaux";
+
+let country = "Bordeaux"
+// arr = "Une liste de nombres";
+
+const numbersList = [1,2,3,4]
+
+// ?? = "le prénom du plus grand agent secret de tous les temps"; 
+
+let bestAgentSurName ="le prénom du plus grand agent secret de tous les temps";
