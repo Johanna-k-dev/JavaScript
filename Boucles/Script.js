48594410ae89dab0e5c8a65d122940ce64c5d1d7
Johanna Keïta <veille.technologique.jk@gmail.com>
2024-11-30 17:25:59 +0100

    Created Boucle folder and linked Index.html / script.js

---
+++ Boucles/Script.js
@@ -0,0 +1,47 @@
+/**
+   * -------------------------------------------------------
+   * 1 - Vous êtes un pilote de F1.
+   * 
+   * Créer une boucle qui permette d'afficher "Tour n°X" sur vos 50 tours de circuits
+   * 
+   * Ex : 
+   * Tour n°1
+   * Tour n°2
+   * Tour n°3
+   * etc...
+   * -------------------------------------------------------
+*/
+
+
+
+ /**
+   * -------------------------------------------------------
+   * 2 - Vous êtes (encore) un pilote de F1 mais cette fois-ci, vous avez avec vous un copain-pilote avec vous parce que vous avez deux fois plus de tours à faire.
+   * 
+   * Créer une boucle qui affiche tous les tours 🔂
+   * Si vous êtes au premier tour, afficher "Zé bartiii, c'est à conducteur 1️⃣ de démarrer"
+   * Si vous êtes au tour 25, afficher "Il faut changer de conducteur, c'est à conducteur 2️⃣"
+   * Si vous êtes au tour 50, afficher "Il faut changer de conducteur, c'est à conducteur 1️⃣"
+   * Si vous êtes au tour 75, afficher "Il faut changer de conducteur, c'est à conducteur 2️⃣"
+   * Si vous êtes au tour 100, afficher "C'est fini, bien joué à tous, HIGH FIVE ! 🙌😎"
+   * -------------------------------------------------------
+*/
+
+
+
+ /**
+   * -------------------------------------------------------
+   * 3 - Vous êtes (toujours) un pilote de F1, sur 101 tours. Vous devez maintenant faire attention à votre essence. 
+   * 
+   * Votre réserve de carburant est de 74L.
+   * Chaque tour consomme 7L.
+   * 
+   * Créer une boucle qui affiche tous les tours et le fuel restant à la fin de chaque tour 🔂. Ex : "Tour n°88, Fuel restant : 19"
+   * Si vous allez être à court de carburant au prochain tour : 
+   *    afficher en warning : "Attention carburant à recharger au prochain tour⛽️"
+   *    recharger le carburant le tour suivant
+   * Une fois le carburant rechargé, afficher en warning : "Le refuel a été fait 🙌😎"
+   * Une fois la course terminée, afficher le nombre total de refuel qui aura été nécéssaire. Ex : "Nombre total de refuel :  2"
+   * Vous devez obtenir le résultat de la capture d'écran "boucles-basiques-resultat"
+   * -------------------------------------------------------
+*/
