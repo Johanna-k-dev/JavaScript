5270c8d65ec21ca195ed6c7d6beb75d408154880
Johanna Keïta <veille.technologique.jk@gmail.com>
2024-12-01 03:01:23 +0100

    ended Selectionner-elements exercise

--- Selectionner-elements/selectionner-element-exos.js
+++ Selectionner-elements/selectionner-element-exos.js
@@ -5,7 +5,11 @@
    * → par son nom de balise
    * -------------------------------------------------------
 */
+console.log("Exercice 1");
 
+const ourServicesSection = document.getElementById('our-services');
+
+console.log(ourServicesSection);
 
 /**
    * -------------------------------------------------------
@@ -13,6 +17,13 @@
    * → Afficher leur valeur (value) dans la console
    * -------------------------------------------------------
 */
+console.log("Exercice 2");
+
+const input1 = document.getElementById('great');
+const input1Value = input1.value;
+const input2 = document.getElementById('not-great');
+const input2Value = input2.value;
+console.log(`Value input1:  ${input1Value} Value input2: ${input2Value}`);
 
 /**
    * -------------------------------------------------------
@@ -20,7 +31,12 @@
    * → afficher la valeur de son texte dans la console
    * -------------------------------------------------------
 */
+console.log("Exercice 3");
 
+const sections = document.querySelectorAll('section');  
+const divSecondSection = sections[1].querySelectorAll('div');  
+const h3SecondDiv = divSecondSection[1].querySelector('h3');  
+console.log(h3SecondDiv.textContent); 
 
 /**
    * -------------------------------------------------------
@@ -37,11 +53,38 @@
    * -------------------------------------------------------
 */
 
+console.log("Exercice 4");
+// Selection de toutes les classes
+
+const btnClassElement =document.getElementsByClassName('btn');
+const ctaBtnClassElement =document.getElementsByClassName('cta-btn');
+const ctaPrimaryClassElement =document.getElementsByClassName('cta-primary');
+const ctaSecondaryClassElement=document.getElementsByClassName('cta-secondary');
+const ctaBtnCtaPrimaryClassElement =document.getElementsByClassName('cta-btn','cta-primary');
+const btnCtaBtnCtaSecondaryClassElement =document.getElementsByClassName('btn','cta-btn','cta-secondary');
+
+// Initialiser un compteur btn
+
+let btnCounter = btnClassElement.length;
+
+console.log(btnCounter);
+console.log(ctaBtnCtaPrimaryClassElement);
+
+
+
 /**
    * -------------------------------------------------------
    * 5 - Récupérer tous les paragraphes. Via une boucle, les afficher dans la console
    * -------------------------------------------------------
 */
+console.log("Exercice 5");
+const p = document.querySelectorAll('p');
+
+for (let i = 0; i < p.length; i++) {
+   console.log(p[i]);
+}
+
+
 
 /**
    * -------------------------------------------------------
@@ -54,6 +97,17 @@
    * 
    * -------------------------------------------------------
 */
+
+console.log("Exercice 6");
+
+
+const FirstSection = sections[0];
+console.log(FirstSection.parentElement);
+console.log(FirstSection.firstChild);
+console.log(FirstSection.lastChild);
+console.log(ourServicesSection.previousElementSibling);
+console.log(ourServicesSection.nextElementSibling);
+
 
 /**
    * -------------------------------------------------------
@@ -68,3 +122,58 @@
    * → Calculer la somme totale de ces occurences et afficher le résultat dans la console
    * -------------------------------------------------------
 */
+
+console.log("Exercice 7");
+//Selectionner les balises
+
+const h1 = document.querySelectorAll('h1');
+const h2  = document.querySelectorAll('h2');
+const h3 = document.querySelectorAll('h3');
+//const p = document.querySelectorAll('p'); (déclarées plus haut)
+//const sections = document.querySelectorAll('section');
+const div = document.querySelectorAll('div');
+
+// Initialiser et formater les donées
+
+const elementsSearch = [
+{
+   name: "h1",
+   length: h1.length
+},
+{
+   name:  "h2",
+   length:h2.length
+},{
+   name:"h3"  ,
+   length:h3.length
+},{
+   name: "p",
+   length:p.length
+},{
+   name: "sections" ,
+   length:sections.length
+},{
+   name: "div" ,
+   length:div.length
+}];
+   
+console.log(elementsSearch);
+function elementsDomCounter() {
+   //Boucle
+
+   for (let i = 0; i < elementsSearch.length; i++) {
+
+      const element = elementsSearch[i];
+      
+      console.log(`Il y a ${element.length} éléments ${element.name}.`);
+         
+   }
+   
+   const totalElements = elementsSearch.reduce((sum, element) => sum + element.length, 0);
+   console.log(`La somme totale des longueurs est : ${totalElements}`)
+
+}
+elementsDomCounter()
+ 
+
+
